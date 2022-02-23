19cadb7a3d8da3874dfb1e954ce5b4929c7ca0e1
jonathandowdell <jdowde2@wgu.edu>
2022-02-23 13:04:31 -0600

    OffSet

--- Sources/Charts/View/Pie/DonutChart.swift
+++ Sources/Charts/View/Pie/DonutChart.swift
@@ -63,9 +63,9 @@ public struct DonutChart<T>: View where T: CircleChartViewModel {
         }
     }
     
-    func getLabelCoordinate(in geoSize: CGSize, for degree: Double) -> CGPoint {
+    func getLabelCoordinate(in geoSize: CGSize, for degree: Double, with offSet: Double) -> CGPoint {
         let center = CGPoint(x: geoSize.width / 2, y: geoSize.height / 2)
-        let radius = (geoSize.width / 3) + 75
+        let radius = (geoSize.width / 3) + 75 + offSet
         let radian = CGFloat(degree) * (CGFloat.pi / 180)
         let yCoordinate = radius * sin(radian)
         let xCoordinate = radius * cos(radian)
